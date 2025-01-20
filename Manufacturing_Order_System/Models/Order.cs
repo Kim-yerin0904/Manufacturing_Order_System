0224b53d12abec324f9060133cf06f6f0ee6e58a
Kim-yerin0904 <yeelin0904@naver.com>
2025-01-20 12:23:14 +0900

    init : create Model class, directory

---
+++ Manufacturing_Order_System/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Manufacturing_Order_System.Models
+{
+    public class Order : ObservableObject
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductTypeId { get; set; }
+        public int OrderQuantity { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateOnly OrderDueDate { get; set; }
+        public int OrderStatus { get; set; }
+        public DateTime OrderReceiptDate { get; set; }
+    }
+
+}
---
+++ Manufacturing_Order_System/Models/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manufacturing_Order_System.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public int OrderId { get; set; }
+        public int TaskProductionQuantity { get; set; }
+        public int TaskCompletedQuantity { get; set; }
+        public int TaskteamId { get; set; }
+    
+
+        public double ProgressRate
+        {
+            get
+            {
+                return TaskProductionQuantity > 0
+                    ? Math.Round((double)TaskCompletedQuantity / TaskProductionQuantity * 100, 2)
+                    : 0.0;
+            }
+        }
+    }
+}
