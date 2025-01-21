6afb6b217ff1735ab1360acc8b4ce3c08dd65cba
Kim-yerin0904 <yeelin0904@naver.com>
2025-01-21 10:05:12 +0900

    feat workinginfo & DailyReport

---
+++ Manufacturing_Order_System/ViewModels/WorkingInfoViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Manufacturing_Order_System.Models;
+using Manufacturing_Order_System.ViewModels.Bases;
+
+namespace Manufacturing_Order_System.ViewModels
+{
+    public class WorkingInfoViewModel : ViewModelBase
+    {
+        public ObservableCollection<Order> orders { get; set; }
+        public ObservableCollection<Models.Task> tasks { get; set; }
+        public ObservableCollection<Taskteam> taskteams { get; set; }
+        public ObservableCollection<Worker> workers { get; set; }
+
+        private Models.Task _selectedTask;
+        public Models.Task SelectedTask
+        {
+            get { return _selectedTask; }
+            set
+            {
+                _selectedTask = value;
+                OnPropertyChanged(nameof(SelectedTask));
+            }
+        }
+
+        public WorkingInfoViewModel()
+        {
+            orders = new ObservableCollection<Order>();
+            tasks = new ObservableCollection<Models.Task>();
+            taskteams = new ObservableCollection<Taskteam> { };
+            workers = new ObservableCollection<Worker>();
+        }
+
+        
+
+
+    }
+}
