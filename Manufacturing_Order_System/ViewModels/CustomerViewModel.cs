af334868c24e2f918d2484cf1d2d516b629b8e55
kknyapple <kknyapple@gmail.com>
2025-01-22 09:46:47 +0900

    feat: OrderPage

---
+++ Manufacturing_Order_System/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manufacturing_Order_System.ViewModels
+{
+    public partial class CustomerViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string customerName;
+
+        [ObservableProperty]
+        private string customerContact;
+
+        [ObservableProperty]
+        private string customerEmail;
+
+        [ObservableProperty]
+        private string customerAddress;
+    }
+}
